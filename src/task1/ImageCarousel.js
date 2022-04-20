bf7cb81a942e9f660c4bb7795e3354016b1c1f46
bkarkusashvili <beqa.qarqusashvili.20@gmail.com>
2022-04-20 13:17:11 +0400

    finish tasks

--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -1,7 +1,70 @@
-import React from "react";
-import { fetchImageUrls } from "../api/index";
+import React, { useState, useEffect, useMemo } from "react";
+import { fetchImage, fetchImageUrls } from "../api/index";
+import { CircularProgress, Box, Stack, Button } from '@mui/material';
 
 const ImageCarousel = (props) => {
-    return <h1>Your code goes here</h1>;
+    const [total, setTotal] = useState(0);
+    const [index, setIndex] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [currentLoaded, setCurrentLoaded] = useState(false);
+    const [activeImage, setActiveImage] = useState();
+
+    const nextSlide = useMemo(() => index === total - 1 ? 0 : index + 1, [index, total]);
+    const prevSlide = useMemo(() => index === 0 ? total - 1 : index - 1, [index, total]);
+
+    const loadNextImages = () => {
+        fetchImage(nextSlide)
+            .then(() => console.log('Next Loaded'))
+            .catch((err) => console.error(err));
+
+        fetchImage(prevSlide)
+            .then(() => console.log('Prev Loaded'))
+            .catch((err) => console.error(err));
+    };
+
+    useEffect(() => {
+        setLoading(true);
+        fetchImageUrls()
+            .then((res) => setTotal(res.length))
+            .catch((err) => console.error(err))
+            .finally(() => setLoading(false));
+    }, []);
+
+    useEffect(() => {
+        if (!total) return;
+
+        setCurrentLoaded(false);
+        fetchImage(index)
+            .then((res) => {
+                setCurrentLoaded(true);
+                setActiveImage(res);
+                loadNextImages();
+            })
+            .catch((err) => console.error(err))
+    }, [index, total]);
+
+    return (
+        <Box sx={{ display: 'flex', marginTop: 20 }} justifyContent={'center'} p={3}>
+            {loading ?
+                <CircularProgress /> :
+                (
+                    <Box sx={{ width: '500px' }}>
+                        <Stack direction="row" justifyContent={'space-between'} spacing={2} mb={2}>
+                            <Button variant="outlined" onClick={() => setIndex(prevSlide)}>
+                                Prev
+                            </Button>
+                            <Button variant="contained" onClick={() => setIndex(nextSlide)}>
+                                Next
+                            </Button>
+                        </Stack>
+                        {activeImage && currentLoaded ?
+                            <img src={activeImage} width={500} height={400} style={{ objectFit: 'cover' }} /> :
+                            <CircularProgress />
+                        }
+                    </Box>
+                )
+            }
+        </Box>
+    );
 };
 export default ImageCarousel;
--- src/task2/ecommerce.js
+++ src/task2/ecommerce.js
@@ -7,23 +7,51 @@ const fetchAllOrders = () => {
     const ids = allIds;
     // .....
     //   1. TODO: fetch all ids using the "fetchOrderById" and the given ids, make it work as efficient and clean as possible.
+
+    return Promise.all(ids.map(id => fetchOrderById(id)));
 };
 
-const bucketOrdersByUsers = () => {
+const bucketOrdersByUsers = async () => {
     let ordersByUsers = {};
     //   2. TODO: using the function from section 1 you should now bucket the orders by user.
     // each key in the object (ordersByUsers) represents a userId and each value is an array of the orders of that user.
+
+    const orders = await fetchAllOrders();
+    orders.forEach(order => {
+        if (!ordersByUsers.hasOwnProperty(order.userId)) {
+            ordersByUsers[order.userId] = [];
+        }
+        ordersByUsers[order.userId].push(order);
+    });
+
     return ordersByUsers;
 };
 
-const getLast2WeeksOrders = () => {
+const getLast2WeeksOrders = async () => {
     //   3. TODO: fetch all Ids and return array with only the last 2 weeks orders. make it work as efficient and clean as possible.
+
+    const current = Date.now();
+    const pastTwoWeek = current - 14 * 24 * 60 * 60 * 1000;
+
+    const orders = await fetchAllOrders();
+
+    return orders.filter(order => order.timestamp >= pastTwoWeek && order.timestamp <= current);
 };
 
-const bucketOrdersByDate = () => {
+const bucketOrdersByDate = async () => {
     let ordersByDate = {};
     //   4. TODO: using the function from section 3 bucket the orders by date.
     // each key in the object (ordersByDate) represents a day and each value is an array of the orders in that date.
+
+    const orders = await fetchAllOrders();
+    orders.forEach(order => {
+        const day = new Date(order.timestamp).getDay();
+        if (!ordersByDate.hasOwnProperty(day)) {
+            ordersByDate[day] = [];
+        }
+        ordersByDate[day].push(order);
+    });
+
     return ordersByDate;
 };
 
